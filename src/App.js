180717cb94956c5ca0e93f9b4aa021af4772c5be
Olanrewaju <activesystemstech@gmail.com>
2021-08-04 20:42:57 +0100

    added cart icon and cart dropdwon component into our application also implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx'
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import { connect } from 'react-redux';
@@ -50,7 +50,7 @@ componentWillUnmount() {
        <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route path='/shop' component={ShopPage}/>
-      <Route path='/signin' component={SignInAndSignUpPage}/>
+      <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />): (< SignInAndSignUpPage/>)}/>
       </Switch>
   
     </div>
@@ -60,10 +60,14 @@ componentWillUnmount() {
  }
 
 
+ const mapStateToProps = ({user}) =>({
+   currentUser: user.currentUser
+
+ })
 
  const mapDispatchToProps = dispatch => ({
    setCurrentUser: user => dispatch(setCurrentUser(user))
 
  });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
